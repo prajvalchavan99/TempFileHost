cb410716292866a1ddeb0fb7055f28d59a112ef1
“EMP610” <prajval.chavan@finoit.com>
2024-12-02 12:59:32 +0530

    Minor changes and SEO

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-console.log(">>>>>",BACKEND_URL);
 const api = axios.create({
   baseURL: `${BACKEND_URL}`,
   timeout: 120000,
